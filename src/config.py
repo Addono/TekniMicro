2274a441ac4b27d2e7cd075a54ebae5914c2ecb6
Adriaan Knapen <hi@aknapen.nl>
2020-03-18 09:35:30 +0100

    🥅 Catch illegal JSON files

--- src/config.py
+++ src/config.py
@@ -1,5 +1,9 @@
 import json
 
 def load_config():
+  try:
     with open("/config.json", "r") as config_file:
       return json.load(config_file)
+  except ValueError:
+    print("Illegal JSON file recieved")
+    return {}
