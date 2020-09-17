290129dd2d006c66c7825cd4eb3819014f7c6d4a
Adriaan Knapen <hi@aknapen.nl>
2020-09-17 11:31:27 +0200

    style: reformat config generator using black

--- create_config.py
+++ create_config.py
@@ -13,15 +13,23 @@ with open(PATH, "r+" if file_exists else "w") as config_file:
         print(e)
         config = {}
 
-    for field in ['WIFI_SSID', 'WIFI_PASSWORD', 'MQTT_HOSTNAME', 'MQTT_USERNAME', 'MQTT_PASSWORD']:
+    for field in [
+        "WIFI_SSID",
+        "WIFI_PASSWORD",
+        "MQTT_HOSTNAME",
+        "MQTT_USERNAME",
+        "MQTT_PASSWORD",
+    ]:
         if field not in config:
             value = input(f"Please enter the desired value for {field}:\n")
             config[field] = value
 
     # Add a random seed
     config["MQTT_CLIENT_ID"] = "micro-" + str(random.randint(0, 10000000))
-    
+
     # Write the config back to the file
-    config_file.seek(0)  # Move the filepointer back to the start, otherwise the new config will be appended
+    config_file.seek(
+        0
+    )  # Move the filepointer back to the start, otherwise the new config will be appended
     json.dump(config, config_file)
-    config_file.truncate() # Remove any remaining leftover present after overwriting
+    config_file.truncate()  # Remove any remaining leftover present after overwriting
